def4c8ad98206f92cab9bff5d048d7f63c6d2bac
armariya <arm.armariya@gmail.com>
2020-02-26 19:33:00 +0700

    test: add more for specific account

--- test/promptpay_test.dart
+++ test/promptpay_test.dart
@@ -7,7 +7,15 @@ void main() {
   test('toString 3 must be 03', () {
     expect(3.toString().padLeft(2, '0'), "03");
   });
-  test('should generate promptpay data for identity correctly', () {
+  test('should generate promptpay data for phone number correctly', () {
     expect(PromptPay.generateQRData("0000000000", 123.45), "00020101021129370016A000000677010111011300660000000005802TH53037645406123.456304b0b2");
+  });
+
+  test('should generate promptpay data for identity correctly', () {
+    expect(PromptPay.generateQRData("0000000000000", 123.45), "00020101021129370016A000000677010111021300000000000005802TH53037645406123.4563046dcc");
+  });
+
+  test('should generate promptpay data for e-wallet correctly', () {
+    expect(PromptPay.generateQRData("000000000000000", 123.45), "00020101021129370016A00000067701011103150000000000000005802TH53037645406123.456304ae16");
   });
 }
